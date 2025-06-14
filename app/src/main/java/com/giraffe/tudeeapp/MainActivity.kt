cee647fb6bcbf9cf5bd5c64ebc93bf5e80f3568c
Ashraf El-Fallah <108183484+Ashraf-El-Fallah@users.noreply.github.com>
2025-06-14 15:40:27 +0300

    add slider component to design system (#4)
    Co-authored-by: Mohamed Ebrahim <111383089+mohamedebrahem13@users.noreply.github.com>

--- app/src/main/java/com/giraffe/tudeeapp/MainActivity.kt
+++ app/src/main/java/com/giraffe/tudeeapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.giraffe.tudeeapp
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.giraffe.tudeeapp.design_system.theme.Theme
 import com.giraffe.tudeeapp.design_system.theme.TudeeTheme
 
 class MainActivity : ComponentActivity() {
