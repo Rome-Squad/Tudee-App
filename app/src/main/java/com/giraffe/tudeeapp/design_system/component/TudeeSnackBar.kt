ac0cedcd5549429409e112233e038e68eccdaafe
bilalazzam <blalazam065@gmail.com>
2025-06-14 13:10:35 +0300

    add TudeeSnackBar

---
+++ app/src/main/java/com/giraffe/tudeeapp/design_system/component/TudeeSnackBar.kt
@@ -0,0 +1,92 @@
+package com.giraffe.tudeeapp.design_system.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.giraffe.tudeeapp.design_system.theme.Theme
+
+@Composable
+fun TudeeSnackBar(
+    modifier: Modifier = Modifier,
+    message: String,
+    iconRes: Int,
+    backgroundColor: Color = Theme.color.surfaceHigh,
+    textColor: Color = Color(0xFF1F1F1F),
+    iconTintColor: Color = Theme.color.error,
+    iconBackgroundColor: Color = Theme.color.errorVariant
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .shadow(
+                elevation = 16.dp,
+                shape = RoundedCornerShape(16.dp),
+                ambientColor = Color.Black.copy(alpha = 0.12f)
+            )
+            .background(backgroundColor, RoundedCornerShape(16.dp))
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .background(iconBackgroundColor),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = iconRes),
+                contentDescription = null,
+                tint = iconTintColor,
+                modifier = Modifier.size(21.5.dp)
+            )
+        }
+
+        Text(
+            text = message,
+            color = textColor,
+            style = Theme.textStyle.body.medium
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TudeeSnackBarPreview() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(40.dp),
+        verticalArrangement = Arrangement.spacedBy(40.dp),
+    ) {
+        TudeeSnackBar(
+            message = "Error message",
+            iconRes = com.giraffe.tudeeapp.R.drawable.ic_error
+        )
+
+        TudeeSnackBar(
+            message = "Success message",
+            iconRes = com.giraffe.tudeeapp.R.drawable.ic_success,
+            iconTintColor = Theme.color.greenAccent,
+            iconBackgroundColor = Theme.color.greenVariant
+        )
+    }
+}
