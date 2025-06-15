05864fcca95d4fafdb704ad747e8b3450f8c49f1
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-06-15 14:05:13 +0300

    add splash route and navigation composable,create splash screen

--- app/src/main/java/com/giraffe/tudeeapp/MainActivity.kt
+++ app/src/main/java/com/giraffe/tudeeapp/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.giraffe.tudeeapp.design_system.component.NavBar
 import com.giraffe.tudeeapp.design_system.theme.TudeeTheme
 import com.giraffe.tudeeapp.presentation.categories.CategoriesScreen
 import com.giraffe.tudeeapp.presentation.home.HomeScreen
+import com.giraffe.tudeeapp.presentation.splash.SplashScreen
 import com.giraffe.tudeeapp.presentation.tasks.TaskScreen
 
 class MainActivity : ComponentActivity() {
@@ -24,15 +26,41 @@ class MainActivity : ComponentActivity() {
         setContent {
             TudeeTheme {
                 val navController = rememberNavController()
+                val currentBackStackEntry = navController.currentBackStackEntryAsState()
+                val currentRoute = currentBackStackEntry.value?.destination?.route
+
+                val showBottomBar = currentRoute != Screen.SplashScreen.route
+
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    bottomBar = { NavBar(navController = navController) }
+                    bottomBar = {
+                        if (showBottomBar) {
+                            NavBar(navController = navController)
+                        }
+                    }
                 ) { innerPadding ->
+
+                    val paddingModifier = if (currentRoute == Screen.SplashScreen.route) {
+                        Modifier.fillMaxSize()
+                    } else {
+                        Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    }
+
                     NavHost(
                         navController = navController,
-                        startDestination = "homeScreen",
-                        modifier = Modifier.padding(innerPadding)
+                        startDestination = Screen.SplashScreen.route,
+                        modifier = paddingModifier
                     ) {
+                        composable(Screen.SplashScreen.route) {
+                            SplashScreen(onTimeout = {
+                                navController.navigate(Screen.HomeScreen.route) {
+                                    popUpTo(Screen.SplashScreen.route) { inclusive = true }
+                                }
+                            })
+                        }
+
                         composable(Screen.HomeScreen.route) {
                             HomeScreen()
                         }
--- app/src/main/java/com/giraffe/tudeeapp/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/splash/SplashScreen.kt
@@ -1,2 +1,65 @@
 package com.giraffe.tudeeapp.presentation.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.giraffe.tudeeapp.design_system.theme.Theme
+import com.giraffe.tudeeapp.design_system.theme.TudeeTheme
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(onTimeout: () -> Unit) {
+
+    LaunchedEffect(Unit) {
+        delay(3000)
+        onTimeout()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Theme.color.primary,
+                        Theme.color.surface
+                    ),
+                    start = Offset(0f, 0f),
+                    end = Offset.Infinite
+                )
+            )
+    ) {
+        Image(
+            painter = painterResource(id = Theme.resources.bacgroundImage),
+            contentDescription = "Splash Background",
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
+        )
+
+        Image(
+            painter = painterResource(id = Theme.resources.logoImageResId),
+            contentDescription = "Splash Logo",
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    TudeeTheme {
+        SplashScreen {}
+    }
+}
