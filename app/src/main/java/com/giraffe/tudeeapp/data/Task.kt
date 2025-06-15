8ce0593748744b1003c318247ee88144dc3ea0ca
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-06-15 10:23:56 +0300

    add status in task

--- app/src/main/java/com/giraffe/tudeeapp/data/Task.kt
+++ app/src/main/java/com/giraffe/tudeeapp/data/Task.kt
@@ -2,6 +2,8 @@ package com.giraffe.tudeeapp.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.giraffe.tudeeapp.domain.model.task.TaskPriority
+import com.giraffe.tudeeapp.domain.model.task.TaskStatus
 import kotlinx.datetime.LocalDateTime
 
 @Entity
@@ -9,8 +11,8 @@ data class Task (
     @PrimaryKey val uid: Long,
     val title: String,
     val description: String,
-//    val taskPriority: TaskPriority,
-//    val status: TaskStatus,
+    val taskPriority: TaskPriority,
+    val status: TaskStatus,
     val categoryId: Long,
     val dueDate: LocalDateTime,
     val createdAt: LocalDateTime,
