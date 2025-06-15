e22809d631c2e0c146fc05f014e798f60e962c17
bilalazzam <blalazam065@gmail.com>
2025-06-15 22:10:23 +0300

    add task ui

--- app/src/main/java/com/giraffe/tudeeapp/presentation/home/taskdetails/TaskUi.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/home/taskdetails/TaskUi.kt
@@ -1,6 +1,7 @@
 package com.giraffe.tudeeapp.presentation.home.taskdetails
 
-import com.giraffe.tudeeapp.domain.model.category.Category
+import com.giraffe.tudeeapp.design_system.component.PriorityType
+import com.giraffe.tudeeapp.domain.model.Category
 import com.giraffe.tudeeapp.domain.model.task.Task
 import com.giraffe.tudeeapp.domain.model.task.TaskPriority
 import com.giraffe.tudeeapp.domain.model.task.TaskStatus
@@ -10,7 +11,7 @@ data class TaskUi (
     val id: Long,
     val title: String,
     val description: String,
-    val taskPriority: TaskPriority,
+    val priorityType: PriorityType,
     val status: TaskStatus,
     val category: Category,
     val dueDate: LocalDateTime,
@@ -22,10 +23,16 @@ fun Task.toTaskUi(category: Category) = TaskUi(
     id = id,
     title = title,
     description = description,
-    taskPriority = taskPriority,
+    priorityType = taskPriority.toPriorityType(),
     status = status,
     category = category,
     dueDate = dueDate,
     createdAt = createdAt,
     updatedAt = updatedAt
 )
+
+fun TaskPriority.toPriorityType() = when(this) {
+    TaskPriority.LOW -> PriorityType.LOW
+    TaskPriority.MEDIUM -> PriorityType.MEDIUM
+    TaskPriority.HIGH -> PriorityType.HIGH
+}
