844fe48a399a2c84338dd1f23373f3c1f7ea330c
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-06-15 10:24:22 +0300

    use flow and suspend

--- app/src/main/java/com/giraffe/tudeeapp/data/database/TaskDao.kt
+++ app/src/main/java/com/giraffe/tudeeapp/data/database/TaskDao.kt
@@ -1,26 +1,34 @@
-package com.giraffe.tudeeapp.data.database
-
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.giraffe.tudeeapp.data.Task
+import com.giraffe.tudeeapp.domain.model.task.TaskStatus
+import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.LocalDateTime
 
 @Dao
 interface TaskDao {
-    @Query("SELECT * FROM task")
-    fun getAll(): List<Task>
 
-    @Query("SELECT * FROM task WHERE uid IN (:userIds)")
-    fun loadAllByIds(userIds: IntArray): List<Task>
+    @Query("SELECT * FROM task WHERE dueDate = :date")
+    fun getTasksByDate(date: LocalDateTime): Flow<List<Task>>
 
-//    @Query("SELECT * FROM task WHERE first_name LIKE :first AND " +
-//            "last_name LIKE :last LIMIT 1")
-//    fun findByName(first: String, last: String): User
+    @Query("SELECT * FROM task WHERE categoryId = :categoryId")
+    fun getTasksByCategory(categoryId: Long): Flow<List<Task>>
+
+    @Query("SELECT * FROM task WHERE uid = :id")
+    suspend fun getTaskById(id: Long): Task
 
     @Insert
-    fun insertAll(vararg users: Task)
+    suspend fun createTask(task: Task): Long
+
+    @Update
+    suspend fun updateTask(task: Task): Long
 
     @Delete
-    fun delete(task: Task)
+    suspend fun deleteTask(id: Long): Long
+
+    @Query("UPDATE task SET status = :newStatus WHERE uid = :id")
+    suspend fun changeStatus(id: Long, newStatus: TaskStatus): Unit
 }
