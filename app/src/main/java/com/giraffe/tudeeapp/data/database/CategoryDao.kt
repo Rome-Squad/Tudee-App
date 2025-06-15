34e5f04abcdcd9bf1d979dd65c4981126dc0702e
Doaa Abdelnaser <doaaabdelnaser151998@gmail.com>
2025-06-15 21:37:20 +0300

    refactor name of category class in data layer to be CategoryEntity

--- app/src/main/java/com/giraffe/tudeeapp/data/database/CategoryDao.kt
+++ app/src/main/java/com/giraffe/tudeeapp/data/database/CategoryDao.kt
@@ -1,11 +1,10 @@
 package com.giraffe.tudeeapp.data.database
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
-import com.giraffe.tudeeapp.data.model.Category
+import com.giraffe.tudeeapp.data.model.CategoryEntity
 import com.giraffe.tudeeapp.data.util.Constants.CATEGORY_TABLE_NAME
 import kotlinx.coroutines.flow.Flow
 
@@ -13,16 +12,16 @@ import kotlinx.coroutines.flow.Flow
 interface CategoryDao {
 
     @Query("SELECT * FROM $CATEGORY_TABLE_NAME")
-    fun getAllCategories(): Flow<List<Category>>
+    fun getAllCategories(): Flow<List<CategoryEntity>>
 
     @Query("SELECT * FROM $CATEGORY_TABLE_NAME WHERE uid= :id")
-    suspend fun getCategoryById(id: Long): Category
+    suspend fun getCategoryById(id: Long): CategoryEntity
 
     @Insert
-    suspend fun createCategory(category: Category): Long
+    suspend fun createCategory(category: CategoryEntity): Long
 
     @Update
-    suspend fun updateCategory(category: Category): Unit
+    suspend fun updateCategory(category: CategoryEntity): Unit
 
     @Query("DELETE From $CATEGORY_TABLE_NAME WHERE uid=:id")
     suspend fun deleteCategory(id: Long): Unit
--- app/src/main/java/com/giraffe/tudeeapp/data/database/CategoryDatabase.kt
+++ app/src/main/java/com/giraffe/tudeeapp/data/database/CategoryDatabase.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.giraffe.tudeeapp.data.model.Category
+import com.giraffe.tudeeapp.data.model.CategoryEntity
 import com.giraffe.tudeeapp.data.util.Constants.CATEGORY_DATABASE_NAME
 
-@Database(entities = [Category::class], version = 1)
+@Database(entities = [CategoryEntity::class], version = 1)
 abstract class CategoryDatabase : RoomDatabase() {
     abstract fun categoryDao(): CategoryDao
 
