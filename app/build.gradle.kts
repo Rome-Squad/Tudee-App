b9a32039c891335ca14a1163a8bd8f779f369eec
bilalazzam <blalazam065@gmail.com>
2025-06-15 22:11:44 +0300

    add ciol dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation(libs.androidx.navigation.compose)
+    implementation("io.coil-kt:coil-compose:2.4.0")
 }
