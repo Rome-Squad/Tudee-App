befe6d7b9a9f2440d70f75a09cc0cfc3a0a07577
bilalazzam <blalazam065@gmail.com>
2025-06-15 22:09:19 +0300

    add safe flow call

--- app/src/main/java/com/giraffe/tudeeapp/data/util/safeCall.kt
+++ app/src/main/java/com/giraffe/tudeeapp/data/util/safeCall.kt
@@ -1,6 +1,10 @@
 package com.giraffe.tudeeapp.data.util
 
 import com.giraffe.tudeeapp.domain.util.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlin.collections.map
 
 suspend fun <T> safeCall(
     block: suspend () -> T
@@ -8,11 +12,24 @@ suspend fun <T> safeCall(
     return try {
         Result.Success(block())
     } catch (e: Throwable) {
-        val error = when (e) {
-            is NoSuchElementException -> NotFoundError()
-            is IllegalArgumentException -> ValidationError()
-            else -> UnknownError()
+        Result.Error(mapExceptionToDomainError(e))
+    }
+}
+
+fun <T> safeFlowCall(block: () -> Flow<T>): Flow<Result<T, DomainError>> {
+    return try {
+        block().map { list ->
+            Result.Success(list)
         }
-        Result.Error(error)
+    } catch (e: Exception) {
+       flow {emit((Result.Error(mapExceptionToDomainError(e))))}
+    }
+}
+
+fun mapExceptionToDomainError(e: Throwable): DomainError {
+    return when (e) {
+        is NoSuchElementException -> NotFoundError()
+        is IllegalArgumentException -> ValidationError()
+        else -> UnknownError()
     }
 }
