bd81a6fbaf2420c76b4785da7474ab874303dcaa
bilalazzam <blalazam065@gmail.com>
2025-06-15 22:11:12 +0300

    add tasks state

--- app/src/main/java/com/giraffe/tudeeapp/presentation/home/taskdetails/TaskDetailsState.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/home/taskdetails/TaskDetailsState.kt
@@ -4,8 +4,7 @@ import com.giraffe.tudeeapp.domain.model.Category
 import com.giraffe.tudeeapp.domain.model.task.Task
 
 data class TaskDetailsState(
-    val task: Task? = null,
-    val taskCategory: Category? = null,
+    val task: TaskUi? = null,
     val isLoading: Boolean = false,
     val error: String? = null,
     val message: String? = null
