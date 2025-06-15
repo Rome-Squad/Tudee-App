5e9711edda5f5e084688cee7a2cd2c3ac779540e
Alaa <alaakhaled9191@gmail.com>
2025-06-15 22:55:46 +0300

    add branch for home screen

--- app/src/main/java/com/giraffe/tudeeapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/home/HomeScreen.kt
@@ -1,16 +1,34 @@
 package com.giraffe.tudeeapp.presentation.home
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.sp
-import com.giraffe.tudeeapp.design_system.theme.Theme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.giraffe.tudeeapp.design_system.component.TudeeAppBar
 
 @Composable
-fun HomeScreen() {
-    Text(
-        text = "Home Screen",
-        color = Theme.color.title,
-        fontSize = 20.sp
-    )
-}
+fun HomeScreen(
+) {
+    Column() {
+        TudeeAppBar(
+            isDarkTheme = true,
+            onThemeSwitchToggle = {}
+        )
 
+    }
+}
+@Preview(
+    widthDp = 360,
+)
+@Composable
+fun Preview() {
+    HomeScreen()
+}
