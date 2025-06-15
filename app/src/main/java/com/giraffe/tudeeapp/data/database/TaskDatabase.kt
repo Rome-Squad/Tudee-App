d01f1516300163ea8aef604b53c5658ca0af2237
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-06-15 09:33:35 +0300

    make database singleton

--- app/src/main/java/com/giraffe/tudeeapp/data/database/TaskDatabase.kt
+++ app/src/main/java/com/giraffe/tudeeapp/data/database/TaskDatabase.kt
@@ -10,4 +10,19 @@ import com.giraffe.tudeeapp.data.Task
 @Database(entities = [Task::class], version = 1)
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
+
+    companion object {
+        private const val DATABASE_NAME = "task_database"
+
+        @Volatile
+        private var instance: TaskDatabase? = null
+
+        fun getInstance(context: Context): TaskDatabase {
+            return instance ?: synchronized(this) { buildDatabase(context).also { instance = it } }
+        }
+
+        fun buildDatabase(context: Context): TaskDatabase {
+            return Room.databaseBuilder(context, TaskDatabase::class.java, DATABASE_NAME).build()
+        }
+    }
 }
