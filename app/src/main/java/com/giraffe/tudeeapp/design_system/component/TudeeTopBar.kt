c299bca190dc7d806b4d44ac8b4eb90fdbae7423
Mohamed Ashraf <ma1261327@gmail.com>
2025-06-15 14:21:23 +0300

    add edit button and label text in TudeeTopBar in design system

--- app/src/main/java/com/giraffe/tudeeapp/design_system/component/TudeeTopBar.kt
+++ app/src/main/java/com/giraffe/tudeeapp/design_system/component/TudeeTopBar.kt
@@ -2,11 +2,12 @@ package com.giraffe.tudeeapp.design_system.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -29,55 +30,93 @@ fun TudeeTopBar(
     modifier: Modifier = Modifier,
     background: Color = Theme.color.surfaceHigh,
     titleColor: Color = Theme.color.title,
-    iconRes: Int = R.drawable.ic_back,
+    iconBackRes: Int = R.drawable.ic_back,
+    iconEditRes: Int = R.drawable.pencil_edit_01,
     iconColor: Color = Theme.color.body,
-    borderColor: Color = Theme.color.stroke
+    borderColor: Color = Theme.color.stroke,
+    withOption: Boolean = false,
+    label: String? = null,
+    labelColor: Color = Theme.color.hint,
+    onClickBack: () -> Unit = {},
+    onClickEdit: () -> Unit = {},
 ) {
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .height(64.dp)
             .padding(horizontal = 16.dp, vertical = 12.dp),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .background(background)
-                .border(
-                    width = 1.dp,
-                    color = borderColor,
-                    shape = CircleShape
-                )
-                .padding(10.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                painter = painterResource(id = iconRes),
-                contentDescription = null,
-                tint = iconColor
+        circleButton(
+            iconRes = iconBackRes,
+            onClick = onClickBack,
+            background = background,
+            iconColor = iconColor,
+            borderColor = borderColor,
+        )
+        Column(modifier = Modifier.padding(start = 12.dp)) {
+            Text(
+                text = title,
+                color = titleColor,
+                style = Theme.textStyle.title.large,
             )
 
+            if (label != null) {
+                Text(
+                    text = label,
+                    color = labelColor,
+                    style = Theme.textStyle.label.small,
+                )
+            }
         }
 
-        Text(
-            text = title,
-            color = titleColor,
-            style = Theme.textStyle.title.large
-        )
-
+        Spacer(Modifier.weight(1f))
+        if (withOption)
+            circleButton(
+                iconRes = iconEditRes,
+                onClick = onClickEdit,
+                background = background,
+                iconColor = iconColor,
+                borderColor = borderColor,
+            )
     }
+}
 
+@Composable
+fun circleButton(
+    iconRes: Int,
+    onClick: () -> Unit = {},
+    background: Color = Theme.color.surfaceHigh,
+    borderColor: Color = Theme.color.stroke,
+    iconColor: Color = Theme.color.body,
+) {
+    Box(
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape)
+            .background(background)
+            .border(
+                width = 1.dp,
+                color = borderColor,
+                shape = CircleShape
+            )
+            .clickable(onClick = { onClick })
+            .padding(10.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            painter = painterResource(id = iconRes),
+            contentDescription = null,
+            tint = iconColor
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun TudeeTopBarPreview() {
     TudeeTopBar(
-        modifier = Modifier
-            .padding(16.dp),
-        title = "Tasks"
+        title = "Tasks",
+        withOption = true,
+        //label = "32 Task"
     )
 }
