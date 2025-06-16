d1bb61577a3631fa3494d4fb18d817ea07777196
Ashraf-El-Fallah <elfallahashraf44@gmail.com>
2025-06-16 14:31:28 +0300

    finish home view model with ui state

--- app/src/main/java/com/giraffe/tudeeapp/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/home/HomeViewModel.kt
@@ -1,7 +1,75 @@
 package com.giraffe.tudeeapp.presentation.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.giraffe.tudeeapp.domain.model.task.TaskStatus
+import com.giraffe.tudeeapp.domain.service.CategoriesService
+import com.giraffe.tudeeapp.domain.service.TasksService
+import com.giraffe.tudeeapp.domain.util.Result
+import com.giraffe.tudeeapp.presentation.home.uistate.TasksUiState
+import com.giraffe.tudeeapp.presentation.home.uistate.toUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 
-class HomeViewModel() : ViewModel() {
+class HomeViewModel(
+    private val tasksService: TasksService,
+    private val categoryService: CategoriesService
+) : ViewModel() {
+    private var _tasksUiState = MutableStateFlow(TasksUiState())
+    val tasksUiState: StateFlow<TasksUiState> = _tasksUiState.asStateFlow()
+
+
+//    init {
+//        getAllTasks()
+//    }
+
+
+    private suspend fun getAllTasks() {
+        _tasksUiState.update { it.copy(isLoading = true, errorMessage = null) }
+
+        tasksService.getAllTasks().onEach { result ->
+            _tasksUiState.update { currentState ->
+                when (result) {
+                    is Result.Success -> {
+                        val tasks = result.data
+                        val tasksUiList = tasks.map { task ->
+                            val categoryResult = categoryService.getCategoryById(task.categoryId)
+                            val category = if (categoryResult is Result.Success) {
+                                categoryResult.data
+                            } else null
+
+                            task.toUiState(
+                                categoryImage = category?.imageUri
+                            )
+                        }
+
+                        val todoTasks =
+                            tasksUiList.filter { it.taskStatusUi == TaskStatus.TODO }.take(2)
+
+                        val doneTasks = tasksUiList.filter { it.taskStatusUi == TaskStatus.DONE }
+
+                        val iProgressTasks =
+                            tasksUiList.filter { it.taskStatusUi == TaskStatus.IN_PROGRESS }
+
+                        currentState.copy(
+                            todoTasks = todoTasks,
+                            inProgressTasks = iProgressTasks,
+                            doneTasks = doneTasks,
+                            isLoading = false,
+                            errorMessage = null
+                        )
+                    }
+
+                    is Result.Error -> {
+                        currentState.copy(isLoading = false, errorMessage = result.error)
+                    }
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
 
 }
--- app/src/main/java/com/giraffe/tudeeapp/presentation/home/uistate/TasksUiState.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/home/uistate/TasksUiState.kt
@@ -1,52 +1,53 @@
 package com.giraffe.tudeeapp.presentation.home.uistate
 
+import com.giraffe.tudeeapp.domain.model.category.Category
 import com.giraffe.tudeeapp.domain.model.task.Task
 import com.giraffe.tudeeapp.domain.model.task.TaskPriority
 import com.giraffe.tudeeapp.domain.model.task.TaskStatus
 import com.giraffe.tudeeapp.domain.util.DomainError
 
 data class TasksUiState(
-    val tasks: List<TaskUi>,
+    val todoTasks: List<TaskUi> = emptyList(),
+    val inProgressTasks: List<TaskUi> = emptyList(),
+    val doneTasks: List<TaskUi> = emptyList(),
+    val toDoTasksCount: Int = 0,
+    val inProgressTasksCount: Int = 0,
+    val doneTasksCount: Int = 0,
     val isLoading: Boolean = true,
     val errorMessage: DomainError? = null
 )
 
+
 data class TaskUi(
     val id: String,
-//    val imageUri: String,
-    val taskPriorityUi: TaskPriorityUi,
-    val taskStatusUi: TaskStatusUi,
+    val categoryId: Long,
+    val categoryImage: String?,
+    val taskPriorityUi: TaskPriority,
+    val taskStatusUi: TaskStatus,
     val taskTitle: String,
     val taskDescription: String,
 )
 
-enum class TaskStatusUi { TODO, IN_PROGRESS, DONE }
 
-enum class TaskPriorityUi { LOW, MEDIUM, HIGH }
-
-fun TaskPriority.toUiState(): TaskPriorityUi {
-    return when (this) {
-        TaskPriority.LOW -> TaskPriorityUi.LOW
-        TaskPriority.MEDIUM -> TaskPriorityUi.MEDIUM
-        TaskPriority.HIGH -> TaskPriorityUi.HIGH
-    }
-}
-
-fun TaskStatus.toUiState(): TaskStatusUi {
-    return when (this) {
-        TaskStatus.TODO -> TaskStatusUi.TODO
-        TaskStatus.IN_PROGRESS -> TaskStatusUi.IN_PROGRESS
-        TaskStatus.DONE -> TaskStatusUi.DONE
-    }
-}
-
-// task counts and images
-fun Task.toUiState(): TaskUi {
+// task counts
+fun Task.toUiState(categoryImage: String? = null): TaskUi {
     return TaskUi(
         id = id.toString(),
-        taskPriorityUi = taskPriority.toUiState(),
+        taskPriorityUi = taskPriority,
         taskTitle = title,
         taskDescription = description,
-        taskStatusUi = status.toUiState()
+        taskStatusUi = status,
+        categoryId = categoryId,
+        categoryImage = categoryImage
     )
 }
+
+fun Category.toUiState(): CategoryImageUi {
+    return CategoryImageUi(
+        categoryImage = imageUri
+    )
+}
+
+data class CategoryImageUi(
+    val categoryImage: String?
+)
