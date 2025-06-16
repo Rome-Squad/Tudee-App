50d380ae2eb02314a2c1ad345a1276cf498f48fa
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-06-16 21:58:09 +0300

    Rename AddEditTaskScreen to TaskEditorBottomSheet and update dependencies

--- app/src/main/java/com/giraffe/tudeeapp/presentation/home/TaskEditorBottomSheet.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/home/TaskEditorBottomSheet.kt
@@ -21,30 +21,29 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import com.giraffe.tudeeapp.R
 import com.giraffe.tudeeapp.design_system.component.TudeeSnackBar
-import com.giraffe.tudeeapp.presentation.home.componant.AddEditTaskContent
+import com.giraffe.tudeeapp.presentation.home.componant.TaskEditorBottomSheetContent
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.parameter.parametersOf
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddEditTaskScreen(
+fun TaskEditorBottomSheet(
+    taskId: Long? = null,
     onDismissRequest: () -> Unit,
     headerTitle: String,
     saveButtonText: String,
     isEditMode: Boolean,
-    addEditTaskViewModel: AddEditTaskViewModel = viewModel(),
-    categoryViewModel: CategoryViewModel = viewModel()
+    viewModel: TaskEditorBottomSheetViewModel = koinViewModel{ parametersOf(taskId) },
 ) {
-    val taskState by addEditTaskViewModel.taskState.collectAsState()
-    val categories by categoryViewModel.categories.collectAsState()
-    val categoriesLoading by categoryViewModel.isLoading.collectAsState()
-    val isSuccess by addEditTaskViewModel.isSuccess.collectAsState()
-    val error by addEditTaskViewModel.error.collectAsState()
+
+    val taskState by viewModel.taskState.collectAsState()
 
     val scope = rememberCoroutineScope()
     val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
@@ -52,20 +51,22 @@ fun AddEditTaskScreen(
     var snackbarMessage by remember { mutableStateOf<String?>(null) }
     var snackbarType by remember { mutableStateOf<SnackbarType?>(null) }
 
-    LaunchedEffect(isSuccess) {
-        if (isSuccess) {
-            snackbarMessage = if (isEditMode) "Edited task successfully." else "Add task successfully."
+    // handle success save
+    LaunchedEffect(taskState.isSuccessSave) {
+        if (taskState.isSuccessSave) {
+            snackbarMessage = if (isEditMode) "Edited task successfully." else "Added task successfully."
             snackbarType = SnackbarType.SUCCESS
 
             kotlinx.coroutines.delay(500)
             scope.launch { bottomSheetState.hide() }
             onDismissRequest()
-            addEditTaskViewModel.resetSuccess()
+            viewModel.resetSuccess()
         }
     }
 
-    LaunchedEffect(error) {
-        error?.let {
+    // handle error save
+    LaunchedEffect(taskState.errorMessageSave) {
+        taskState.errorMessageSave?.let {
             snackbarMessage = "Some error happened"
             snackbarType = SnackbarType.ERROR
         }
@@ -77,28 +78,29 @@ fun AddEditTaskScreen(
             scope.launch { bottomSheetState.hide() }
         },
         sheetState = bottomSheetState,
-        modifier = Modifier.width(360.dp).height(690.dp),
+        modifier = Modifier
+            .width(360.dp)
+            .height(690.dp),
     ) {
-        AddEditTaskContent(
+        TaskEditorBottomSheetContent(
             headerTitle = headerTitle,
             saveButtonText = saveButtonText,
             taskState = taskState,
-            categories = categories,
-            isLoading = taskState.isLoading,
-            categoriesLoading = categoriesLoading,
-            onTitleChange = { addEditTaskViewModel.onTitleChange(it) },
-            onDescriptionChange = { addEditTaskViewModel.onDescriptionChange(it) },
-            onPriorityChange = { addEditTaskViewModel.onPriorityChange(it) },
-            onStatusChange = { addEditTaskViewModel.onStatusChange(it) },
-            onCategoryChange = { addEditTaskViewModel.onCategoryChange(it) },
+            categories = taskState.categories,
+            isLoading = taskState.isLoadingTask ,
+            categoriesLoading = taskState.isLoadingCategories,
+            onTitleChange = { viewModel.onTitleChange(it) },
+            onDescriptionChange = { viewModel.onDescriptionChange(it) },
+            onPriorityChange = { viewModel.onPriorityChange(it) },
+            onCategoryChange = { viewModel.onCategoryChange(it) },
             onDueDateChange = { millis ->
                 val localDateTime = millis?.let {
                     Instant.fromEpochMilliseconds(it).toLocalDateTime(TimeZone.currentSystemDefault())
                 } ?: Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
 
-                addEditTaskViewModel.onDueDateChange(localDateTime)
+                viewModel.onDueDateChange(localDateTime)
             },
-            onSaveClick = { addEditTaskViewModel.saveTask() },
+            onSaveClick = { viewModel.saveTask() },
             onCancelClick = {
                 scope.launch { bottomSheetState.hide() }
                 onDismissRequest()
@@ -116,9 +118,9 @@ fun AddEditTaskScreen(
                 TudeeSnackBar(
                     message = message,
                     iconRes = when (snackbarType) {
-                        SnackbarType.SUCCESS -> com.giraffe.tudeeapp.R.drawable.ic_success
-                        SnackbarType.ERROR -> com.giraffe.tudeeapp.R.drawable.ic_error
-                        else -> com.giraffe.tudeeapp.R.drawable.ic_error
+                        SnackbarType.SUCCESS -> R.drawable.ic_success
+                        SnackbarType.ERROR -> R.drawable.ic_error
+                        else -> R.drawable.ic_error
                     },
                 )
             }
@@ -135,5 +137,3 @@ fun AddEditTaskScreen(
 enum class SnackbarType {
     SUCCESS, ERROR
 }
-
-
