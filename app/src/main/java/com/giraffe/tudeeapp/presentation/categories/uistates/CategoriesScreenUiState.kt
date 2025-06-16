49dc667416a8b73c0cbfc97a449d6153dd94d889
Mohamed Ashraf <ma1261327@gmail.com>
2025-06-16 12:43:39 +0300

    add blue color in return with categories icon

--- app/src/main/java/com/giraffe/tudeeapp/presentation/categories/uistates/CategoriesScreenUiState.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/categories/uistates/CategoriesScreenUiState.kt
@@ -1,6 +1,6 @@
 package com.giraffe.tudeeapp.presentation.categories.uistates
 
-import com.giraffe.tudeeapp.R
+import androidx.compose.runtime.Composable
 import com.giraffe.tudeeapp.domain.model.category.Category
 import com.giraffe.tudeeapp.domain.util.DomainError
 import com.giraffe.tudeeapp.presentation.utils.categoriesIcon
@@ -22,12 +22,13 @@ data class CategoryUi(
     val image: Int,
 )
 
+@Composable
 fun Category.toUiState(countTasks: Int): CategoryUi {
     return CategoryUi(
         id = id,
         name = name,
         taskCount = countTasks,
         imageUri = imageUri,
-        image = categoriesIcon[name] ?: R.drawable.developer,
+        image = categoriesIcon(name).categoryImage,
     )
 }
