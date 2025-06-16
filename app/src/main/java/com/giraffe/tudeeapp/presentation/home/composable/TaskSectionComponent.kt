4c18d518331794454d5df0cb6dce8701dc9b2ed9
Ashraf-El-Fallah <elfallahashraf44@gmail.com>
2025-06-16 10:07:03 +0300

    add lazy row for task section component

--- app/src/main/java/com/giraffe/tudeeapp/presentation/home/composable/TaskSectionComponent.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/home/composable/TaskSectionComponent.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -18,6 +22,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.giraffe.tudeeapp.R
+import com.giraffe.tudeeapp.design_system.component.PriorityType
+import com.giraffe.tudeeapp.design_system.component.TaskCard
+import com.giraffe.tudeeapp.design_system.component.TaskCardType
 import com.giraffe.tudeeapp.design_system.theme.Theme
 
 @Composable
@@ -26,7 +33,7 @@ fun TaskSectionComponent(
     numberOfTasks: String = "12",
     taskStatus: String = "In Progress",
 ) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier.height(266.dp)) {
         Row(
             modifier = Modifier
                 .padding(horizontal = 16.dp)
@@ -63,13 +70,49 @@ fun TaskSectionComponent(
             }
 
         }
+        LazyHorizontalGrid(
+            rows = GridCells.Fixed(2),
+            contentPadding = PaddingValues(
+                start = 16.dp,
+                end = 16.dp,
+                top = 8.dp,
+                bottom = 24.dp
+            ),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(count = numberOfTasks.toInt()) {
+                TaskCard(
+                    modifier = Modifier.fillMaxWidth(),
+                    taskIcon = painterResource(R.drawable.birthday_cake_icon),
+                    blurColor = Theme.color.pinkAccent.copy(alpha = .08f),
+                    priority = PriorityType.HIGH,
+                    taskTitle = "Organize Study Desk",
+                    taskDescription = "Review cell structure and functions for tomorrow...",
+                    date = "12-03-2025",
+                    taskCardType = TaskCardType.CATEGORY
+                )
+            }
+        }
+//        Box {
+//            FabButton(
+//                modifier = Modifier.align(Alignment.BottomEnd),
+//                isLoading = false,
+//                isDisable = false,
+//                icon = painterResource(R.drawable.add_task_icon)
+//            ) { }
+//        }
     }
 }
 
 @Preview
 @Composable
 fun TaskSectionComponentPreview() {
-    Box(modifier = Modifier.background(Color.White)) {
+    Box(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxWidth()
+    ) {
         TaskSectionComponent()
     }
 }
