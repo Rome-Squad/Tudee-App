ceb4633ec9beb9790bc4caa4006e2a34901879dd
Ashraf-El-Fallah <elfallahashraf44@gmail.com>
2025-06-17 11:05:13 +0300

    add data class to handle cards over view

--- app/src/main/java/com/giraffe/tudeeapp/presentation/home/composable/OverViewSection.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/home/composable/OverViewSection.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -13,35 +14,52 @@ import com.giraffe.tudeeapp.R
 import com.giraffe.tudeeapp.design_system.theme.Theme
 import com.giraffe.tudeeapp.presentation.home.uistate.TasksUiState
 
+data class TaskCardData(
+    val color: Color,
+    val icon: Int,
+    val taskCount: Int,
+    val taskStatus: String
+)
+
 @Composable
 fun OverViewSection(modifier: Modifier = Modifier, tasksState: TasksUiState) {
     TitleOverView()
+
+    val cardsData = listOf(
+        TaskCardData(
+            color = Theme.color.greenAccent,
+            icon = R.drawable.done_task_card_icon,
+            taskCount = tasksState.doneTasksCount,
+            taskStatus = stringResource(R.string.done_tasks),
+        ),
+        TaskCardData(
+            color = Theme.color.yellowAccent,
+            icon = R.drawable.in_progress_task_card_icon,
+            taskCount = tasksState.inProgressTasksCount,
+            taskStatus = stringResource(R.string.in_progress_tasks)
+        ),
+        TaskCardData(
+            color = Theme.color.purpleAccent,
+            icon = R.drawable.to_do_task_card_icon,
+            taskCount = tasksState.toDoTasksCount,
+            taskStatus = stringResource(R.string.to_do_tasks)
+        )
+    )
+
     Row(
         modifier = modifier
             .fillMaxWidth()
             .padding(start = 12.dp, end = 12.dp, top = 8.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        CardOverView(
-            modifier = Modifier.weight(1f),
-            color = Theme.color.greenAccent,
-            icon = painterResource(R.drawable.done_task_card_icon),
-            taskCount = tasksState.doneTasksCount.toString(),
-            taskStatus = stringResource(R.string.done_tasks),
-        )
-        CardOverView(
-            modifier = Modifier.weight(1f),
-            color = Theme.color.yellowAccent,
-            icon = painterResource(R.drawable.in_progress_task_card_icon),
-            taskCount = tasksState.inProgressTasksCount.toString(),
-            taskStatus = stringResource(R.string.in_progress_tasks)
-        )
-        CardOverView(
-            modifier = Modifier.weight(1f),
-            color = Theme.color.purpleAccent,
-            icon = painterResource(R.drawable.to_do_task_card_icon),
-            taskCount = tasksState.toDoTasksCount.toString(),
-            taskStatus = stringResource(R.string.to_do_tasks)
-        )
+        cardsData.forEach { cardData ->
+            CardOverView(
+                modifier = Modifier.weight(1f),
+                color = cardData.color,
+                icon = painterResource(cardData.icon),
+                taskCount = cardData.taskCount.toString(),
+                taskStatus = cardData.taskStatus
+            )
+        }
     }
 }
