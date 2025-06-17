9b6e366c9455d74a045c1cacf6a5fe16de118e3f
bilalazzam <blalazam065@gmail.com>
2025-06-17 16:06:33 +0300

    add error to message utility

---
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/util/errorToMessage.kt
@@ -0,0 +1,11 @@
+package com.giraffe.tudeeapp.presentation.util
+
+import com.giraffe.tudeeapp.domain.util.DomainError
+import com.giraffe.tudeeapp.domain.util.NotFoundError
+import com.giraffe.tudeeapp.domain.util.ValidationError
+
+fun errorToMessage(error: DomainError): String = when (error) {
+    is NotFoundError -> "Task Not Found!"
+    is ValidationError -> "Sorry, Something Went Wrong"
+    else -> "Sorry, An Unknown Error"
+}
