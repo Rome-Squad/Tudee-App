ac3d2920d896fa3873514db40309d66235399b8e
Ashraf-El-Fallah <elfallahashraf44@gmail.com>
2025-06-17 11:06:32 +0300

    add task status to string resources

--- app/src/main/java/com/giraffe/tudeeapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/home/HomeScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.giraffe.tudeeapp.R
@@ -87,17 +88,17 @@ fun HomeContent(state: TasksUiState) {
                                 .padding(top = 250.dp)
                         ) {
                             TaskSection(
-                                taskStatus = "In Progress",
+                                taskStatus = stringResource(R.string.in_progress),
                                 numberOfTasks = state.inProgressTasksCount.toString(),
                                 tasks = state.inProgressTasks
                             )
                             TaskSection(
-                                taskStatus = "To-Do",
+                                taskStatus = stringResource(R.string.to_do),
                                 numberOfTasks = state.toDoTasksCount.toString(),
                                 tasks = state.todoTasks
                             )
                             TaskSection(
-                                taskStatus = "Done",
+                                taskStatus = stringResource(R.string.done),
                                 numberOfTasks = state.doneTasksCount.toString(),
                                 tasks = state.doneTasks
                             )
