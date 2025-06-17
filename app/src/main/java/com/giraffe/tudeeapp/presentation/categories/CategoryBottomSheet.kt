a158aea581a5603a8ffe81ef66f7bc36847f8328
Mohamed Ashraf <74676105+Mohamed-Ashraf-2210@users.noreply.github.com>
2025-06-17 08:46:23 +0300

    add delete category option and fix save button disabled state

--- app/src/main/java/com/giraffe/tudeeapp/presentation/categories/CategoryBottomSheet.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/categories/CategoryBottomSheet.kt
@@ -49,6 +49,7 @@ import com.giraffe.tudeeapp.design_system.component.button_type.PrimaryButton
 import com.giraffe.tudeeapp.design_system.component.button_type.SecondaryButton
 import com.giraffe.tudeeapp.design_system.theme.Theme
 import com.giraffe.tudeeapp.design_system.theme.TudeeTheme
+import com.giraffe.tudeeapp.presentation.categories.uistates.CategoryUi
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -56,7 +57,11 @@ fun CategoryBottomSheet(
     modifier: Modifier = Modifier,
     title: String = "Add new category",
     isVisible: Boolean = true,
+    categoryToEdit: CategoryUi? = null,
     onVisibilityChange: (Boolean) -> Unit = {},
+    onAddClick: (CategoryUi) -> Unit = {},
+    onEditClick: (CategoryUi) -> Unit = {},
+    onDeleteClick: (CategoryUi) -> Unit = {},
 ) {
     val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
     var categoryTitle by remember { mutableStateOf("") }
@@ -76,15 +81,30 @@ fun CategoryBottomSheet(
         ) {
 
             Column {
-                Text(
+                Box(
                     modifier = Modifier.padding(bottom = 12.dp, start = 16.dp, end = 16.dp),
-                    text = title,
-                    style = Theme.textStyle.title.large,
-                    color = Theme.color.title
-                )
+                ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.CenterStart),
+                        text = title,
+                        style = Theme.textStyle.title.large,
+                        color = Theme.color.title
+                    )
+                    if (categoryToEdit != null) {
+                        Text(
+                            modifier = Modifier
+                                .align(Alignment.CenterEnd)
+                                .clickable(onClick = { onDeleteClick(categoryToEdit) }),
+                            text = "Delete",
+                            style = Theme.textStyle.label.large,
+                            color = Theme.color.error
+                        )
+                    }
+                }
+
                 DefaultTextField(
                     modifier = Modifier.padding(bottom = 12.dp, start = 16.dp, end = 16.dp),
-                    textValue = title,
+                    textValue = categoryToEdit?.name ?: "",
                     onValueChange = { categoryTitle = it },
                     hint = "Category title",
                     iconRes = R.drawable.categories_unselected,
@@ -92,7 +112,7 @@ fun CategoryBottomSheet(
                 Text(
                     modifier = Modifier.padding(bottom = 8.dp, start = 16.dp, end = 16.dp),
                     text = "Category image",
-                    style = Theme.textStyle.title.large,
+                    style = Theme.textStyle.title.medium,
                     color = Theme.color.title
                 )
                 Box(
@@ -112,7 +132,7 @@ fun CategoryBottomSheet(
                         ),
                     contentAlignment = Alignment.Center
                 ) {
-                    if (photoUri == null) {
+                    if (photoUri == null && categoryToEdit?.imageUri == null) {
                         Column(
                             verticalArrangement = Arrangement.Center,
                             horizontalAlignment = Alignment.CenterHorizontally
@@ -145,7 +165,7 @@ fun CategoryBottomSheet(
                             painter = rememberAsyncImagePainter(
                                 ImageRequest
                                     .Builder(LocalContext.current)
-                                    .data(data = photoUri)
+                                    .data(data = photoUri ?: categoryToEdit?.imageUri)
                                     .build()
                             ),
                             contentDescription = "selected photo"
@@ -189,12 +209,31 @@ fun CategoryBottomSheet(
                 ) {
                     PrimaryButton(
                         modifier = Modifier.fillMaxWidth(),
-                        text = "Add",
-                    ) {}
+                        text = if (categoryToEdit != null) "Sava" else "Add",
+                        isDisable = categoryTitle.isBlank() && photoUri == null
+                    ) {
+                        if (categoryToEdit != null) {
+                            onEditClick(
+                                categoryToEdit.copy(
+                                    name = categoryTitle,
+                                    imageUri = photoUri?.toString(),
+                                )
+                            )
+                        } else {
+                            onAddClick(
+                                CategoryUi(
+                                    name = categoryTitle,
+                                    imageUri = photoUri?.toString(),
+                                )
+                            )
+                        }
+                    }
                     SecondaryButton(
                         modifier = Modifier.fillMaxWidth(),
                         text = "Cancel"
-                    ) {}
+                    ) {
+                        onVisibilityChange(false)
+                    }
                 }
             }
         }
