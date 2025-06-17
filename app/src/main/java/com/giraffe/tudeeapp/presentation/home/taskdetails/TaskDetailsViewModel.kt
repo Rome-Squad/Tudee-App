641c5ebdfdc0a9a67485c54a5612f02793977fb2
bilalazzam <blalazam065@gmail.com>
2025-06-17 16:07:15 +0300

    add task details view model

---
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/home/taskdetails/TaskDetailsViewModel.kt
@@ -0,0 +1,78 @@
+package com.giraffe.tudeeapp.presentation.home.taskdetails
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.giraffe.tudeeapp.domain.model.task.TaskStatus
+import com.giraffe.tudeeapp.domain.service.CategoriesService
+import com.giraffe.tudeeapp.domain.service.TasksService
+import com.giraffe.tudeeapp.domain.util.onError
+import com.giraffe.tudeeapp.domain.util.onSuccess
+import com.giraffe.tudeeapp.presentation.util.errorToMessage
+import kotlinx.coroutines.launch
+
+class TaskDetailsViewModel(
+    val taskService: TasksService,
+    val categoryService: CategoriesService
+) : ViewModel() {
+    var taskDetailsState by mutableStateOf<TaskDetailsState>(TaskDetailsState())
+        private set
+
+    fun setTask(task: TaskUi) {
+        taskDetailsState = taskDetailsState.copy(
+            task = task
+        )
+    }
+
+    fun getTaskById(taskId: Long) = viewModelScope.launch {
+        taskDetailsState = taskDetailsState.copy(
+            isLoading = true
+        )
+        taskService.getTaskById(taskId)
+            .onSuccess { task ->
+                categoryService.getCategoryById(task.categoryId)
+                    .onSuccess { category ->
+                        taskDetailsState = taskDetailsState.copy(
+                            task = task.toTaskUi(category),
+                            isLoading = false
+                        )
+                    }
+                    .onError {
+                        taskDetailsState = taskDetailsState.copy(
+                            error = errorToMessage(it)
+                        )
+                    }
+            }
+            .onError {
+                taskDetailsState = taskDetailsState.copy(
+                    error = errorToMessage(it),
+                    isLoading = false
+                )
+            }
+    }
+
+    fun changeTaskStatus(newStatus: TaskStatus) = viewModelScope.launch {
+        taskDetailsState = taskDetailsState.copy(
+            isLoading = true
+        )
+        if (taskDetailsState.task != null) {
+            taskService.changeStatus(taskDetailsState.task!!.id, newStatus)
+                .onSuccess {
+                    taskDetailsState = taskDetailsState.copy(
+                        task = taskDetailsState.task!!.copy(
+                            status = newStatus
+                        ),
+                        isLoading = false
+                    )
+                }
+                .onError {
+                    taskDetailsState = taskDetailsState.copy(
+                        error = errorToMessage(it),
+                        isLoading = false
+                    )
+                }
+        }
+    }
+}
