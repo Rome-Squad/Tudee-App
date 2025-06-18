3c0de06e00c97c95fddd387aae8ba223a2ee9137
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-06-18 19:25:26 +0300

    fix: use TaskPriority

--- app/src/main/java/com/giraffe/tudeeapp/presentation/tasks/viewmodel/TaskUi.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/tasks/viewmodel/TaskUi.kt
@@ -2,8 +2,6 @@ package com.giraffe.tudeeapp.presentation.tasks.viewmodel
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import com.giraffe.tudeeapp.R
-import com.giraffe.tudeeapp.design_system.component.PriorityType
 import com.giraffe.tudeeapp.domain.model.Category
 import com.giraffe.tudeeapp.domain.model.task.Task
 import com.giraffe.tudeeapp.domain.model.task.TaskPriority
@@ -17,7 +15,7 @@ data class TaskUi(
     val id: Long,
     val title: String,
     val description: String,
-    val priorityType: PriorityType,
+    val priorityType: TaskPriority,
     val status: TaskStatus,
     val categoryName: String,
     val dueDate: LocalDateTime,
@@ -27,22 +25,15 @@ data class TaskUi(
 
 @Composable
 fun Task.toTaskUi(category: Category): TaskUi {
-//    val category: Category = getCategoryById(categoryId)
     return TaskUi(
         id = id,
         title = title,
         description = description,
-        priorityType = taskPriority.toPriorityType(),
+        priorityType = taskPriority,
         status = status,
         categoryName = category.name,
         dueDate = dueDate,
         icon = getCategoryIcon(category.name),
         color = getColorForCategoryIcon(category.name)
     )
-}
-
-fun TaskPriority.toPriorityType() = when (this) {
-    TaskPriority.LOW -> PriorityType.LOW
-    TaskPriority.MEDIUM -> PriorityType.MEDIUM
-    TaskPriority.HIGH -> PriorityType.HIGH
 }
