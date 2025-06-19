f9696aad76df7e4a6fae2a0b360b0a8fadaee402
bilalazzam <blalazam065@gmail.com>
2025-06-19 04:14:06 +0300

    refactor tudee snackbar to delay depend on argument

--- app/src/main/java/com/giraffe/tudeeapp/design_system/component/TudeeSnackBar.kt
+++ app/src/main/java/com/giraffe/tudeeapp/design_system/component/TudeeSnackBar.kt
@@ -1,5 +1,9 @@
 package com.giraffe.tudeeapp.design_system.component
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,6 +16,11 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,6 +30,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.giraffe.tudeeapp.design_system.theme.Theme
+import kotlinx.coroutines.delay
 
 @Composable
 fun TudeeSnackBar(
@@ -30,43 +40,62 @@ fun TudeeSnackBar(
     backgroundColor: Color = Theme.color.surfaceHigh,
     textColor: Color = Theme.color.body,
     iconTintColor: Color = Theme.color.error,
-    iconBackgroundColor: Color = Theme.color.errorVariant
+    iconBackgroundColor: Color = Theme.color.errorVariant,
+    durationMillis: Long = 3000L,
+    onDismiss: () -> Unit = {}
 ) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .shadow(
-                elevation = 16.dp,
-                shape = RoundedCornerShape(16.dp),
-                ambientColor = Color.Black.copy(alpha = 0.12f)
-            )
-            .background(backgroundColor, RoundedCornerShape(16.dp))
-            .padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    var visible by remember { mutableStateOf(true) }
+
+    LaunchedEffect (Unit) {
+        delay(durationMillis)
+        visible = false
+        delay(300)
+        onDismiss()
+    }
+
+    AnimatedVisibility(
+        visible = visible,
+        enter = fadeIn(animationSpec = tween(durationMillis = 300)),
+        exit = fadeOut(animationSpec = tween(durationMillis = 300))
     ) {
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .clip(RoundedCornerShape(12.dp))
-                .background(iconBackgroundColor),
-            contentAlignment = Alignment.Center
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .shadow(
+                    elevation = 16.dp,
+                    shape = RoundedCornerShape(16.dp),
+                    ambientColor = Color.Black.copy(alpha = 0.12f)
+                )
+                .background(backgroundColor, RoundedCornerShape(16.dp))
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            Icon(
-                painter = painterResource(id = iconRes),
-                contentDescription = null,
-                tint = iconTintColor,
-                modifier = Modifier.size(21.5.dp)
+            Box(
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(iconBackgroundColor),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = iconRes),
+                    contentDescription = null,
+                    tint = iconTintColor,
+                    modifier = Modifier.size(21.5.dp)
+                )
+            }
+
+            Text(
+                text = message,
+                color = textColor,
+                style = Theme.textStyle.body.medium
             )
         }
-
-        Text(
-            text = message,
-            color = textColor,
-            style = Theme.textStyle.body.medium
-        )
     }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
