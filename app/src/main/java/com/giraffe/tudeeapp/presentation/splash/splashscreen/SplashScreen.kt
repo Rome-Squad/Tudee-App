f341e076b70450dd5cf8d2c1dc3733b5a9106dde
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-06-20 13:19:35 +0300

    use string in splash screen

--- app/src/main/java/com/giraffe/tudeeapp/presentation/splash/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/splash/splashscreen/SplashScreen.kt
@@ -11,9 +11,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.giraffe.tudeeapp.R
 import com.giraffe.tudeeapp.design_system.theme.Theme
 import com.giraffe.tudeeapp.presentation.splash.viewmodel.SplashViewModel
 import kotlinx.coroutines.delay
@@ -51,7 +49,7 @@ fun SplashScreen(
     ) {
         Image(
             painter = painterResource(id = Theme.resources.bacgroundImage),
-            contentDescription = stringResource(R.string.splash_background),
+            contentDescription = Theme.strings.appTagline,
                     modifier = Modifier
                         .fillMaxWidth()
                         .align(Alignment.TopCenter)
@@ -59,7 +57,7 @@ fun SplashScreen(
 
         Image(
             painter = painterResource(id = Theme.resources.logoImageResId),
-            contentDescription = stringResource(R.string.splash_logo),
+            contentDescription = Theme.strings.splashLogo,
             modifier = Modifier.align(Alignment.Center)
         )
     }
