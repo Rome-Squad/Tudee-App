e9da82f68d3dd6e4dd2cd7da87ba4199b636cfd4
Mohannad Ahmed <mohannad199344@gmail.com>
2025-06-23 12:45:36 +0300

    feat(CategoryBottomSheet, Extentions): implement image copying to internal storage

--- app/src/main/java/com/giraffe/tudeeapp/design_system/component/CategoryBottomSheet.kt
+++ app/src/main/java/com/giraffe/tudeeapp/design_system/component/CategoryBottomSheet.kt
@@ -52,6 +52,7 @@ import com.giraffe.tudeeapp.design_system.component.button_type.SecondaryButton
 import com.giraffe.tudeeapp.design_system.theme.Theme
 import com.giraffe.tudeeapp.design_system.theme.TudeeTheme
 import com.giraffe.tudeeapp.domain.model.Category
+import com.giraffe.tudeeapp.presentation.utils.copyImageToInternalStorage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -71,11 +72,7 @@ fun CategoryBottomSheet(
     val launcher =
         rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
             uri?.let {
-                photoUri = it
-                context.contentResolver.takePersistableUriPermission(
-                    it,
-                    Intent.FLAG_GRANT_READ_URI_PERMISSION
-                )
+                photoUri = uri.copyImageToInternalStorage(context)
             }
         }
     if (isVisible) {
