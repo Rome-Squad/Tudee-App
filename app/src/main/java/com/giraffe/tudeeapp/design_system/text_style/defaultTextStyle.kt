ec7590dc0ee804c68690e5357e0937bf1c49db6c
Mohannad Ahmed <mohannad199344@gmail.com>
2025-06-26 21:33:41 +0300

    refactor(defaultTextStyle): add custom font family for improved typography

--- app/src/main/java/com/giraffe/tudeeapp/design_system/text_style/defaultTextStyle.kt
+++ app/src/main/java/com/giraffe/tudeeapp/design_system/text_style/defaultTextStyle.kt
@@ -1,8 +1,17 @@
 package com.giraffe.tudeeapp.design_system.text_style
 
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.giraffe.tudeeapp.R
+
+private val TudeeFontFamily = FontFamily(
+    Font(R.font.nunito, FontWeight.Normal),
+    Font(R.font.nunito_medium, FontWeight.Medium),
+    Font(R.font.nunito_semibold, FontWeight.SemiBold),
+)
 
 val defaultTextStyle = TudeeTextStyle(
     headline = SizedTextStyle(
