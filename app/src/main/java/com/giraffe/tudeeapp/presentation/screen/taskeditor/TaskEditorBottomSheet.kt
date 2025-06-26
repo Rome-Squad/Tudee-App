b173b3d846521fa10b954dd3476798c7dbdfea61
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-06-26 23:22:04 +0300

    use the base bottom sheet within the task editor bottom sheet to encapsulate shared ui components and reduce code duplication.

--- app/src/main/java/com/giraffe/tudeeapp/presentation/screen/taskeditor/TaskEditorBottomSheet.kt
+++ app/src/main/java/com/giraffe/tudeeapp/presentation/screen/taskeditor/TaskEditorBottomSheet.kt
@@ -19,12 +19,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -44,13 +41,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil3.compose.rememberAsyncImagePainter
 import com.giraffe.tudeeapp.R
+import com.giraffe.tudeeapp.design_system.component.BaseBottomSheet
 import com.giraffe.tudeeapp.design_system.component.CategoryItem
 import com.giraffe.tudeeapp.design_system.component.DatePickerDialog
 import com.giraffe.tudeeapp.design_system.component.DefaultTextField
 import com.giraffe.tudeeapp.design_system.component.ParagraphTextField
 import com.giraffe.tudeeapp.design_system.component.Priority
-import com.giraffe.tudeeapp.design_system.component.button_type.PrimaryButton
-import com.giraffe.tudeeapp.design_system.component.button_type.SecondaryButton
 import com.giraffe.tudeeapp.design_system.theme.Theme
 import com.giraffe.tudeeapp.domain.entity.task.TaskPriority
 import com.giraffe.tudeeapp.presentation.utils.EventListener
@@ -80,6 +76,7 @@ fun TaskEditorBottomSheet(
     val scope = rememberCoroutineScope()
     val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
     val taskEditorUiState by viewModel.state.collectAsState()
+
     LaunchedEffect(key1 = taskId, selectedDate) {
         if (taskId == null) {
             viewModel.setDueDate(selectedDate)
@@ -87,6 +84,7 @@ fun TaskEditorBottomSheet(
             viewModel.loadTask(taskId)
         }
     }
+
     EventListener(
         events = viewModel.effect,
     ) { event ->
@@ -96,29 +94,36 @@ fun TaskEditorBottomSheet(
             TaskEditorEffect.TaskEditedSuccess -> onSuccess(context.getString(R.string.task_edited_successfully))
             TaskEditorEffect.DismissTaskEditor -> onDismissRequest()
         }
-
     }
 
-    ModalBottomSheet(
+    BaseBottomSheet(
+        title = if (taskId == null) stringResource(R.string.add_task) else stringResource(R.string.edit_task),
+        isVisible = true,
         onDismissRequest = {
             viewModel.cancel()
             onDismissRequest()
             scope.launch { bottomSheetState.hide() }
         },
         sheetState = bottomSheetState,
-        modifier = modifier.fillMaxHeight(0.95f),
-        containerColor = Theme.color.surface
+        primaryButtonText = if (taskId == null) stringResource(R.string.add) else stringResource(R.string.save),
+        secondaryButtonText = stringResource(R.string.cancel),
+        isPrimaryButtonEnabled = taskEditorUiState.isValidTask,
+        isPrimaryButtonLoading = taskEditorUiState.isLoading,
+        onPrimaryButtonClick = {
+            if (taskId == null) viewModel.addTask(taskEditorUiState.task)
+            else viewModel.editTask(taskEditorUiState.task)
+        },
+        onSecondaryButtonClick = {
+            viewModel.cancel()
+        },
+        modifier = modifier.fillMaxHeight(0.95f)
     ) {
-
         TaskEditorBottomSheetContent(
             taskEditorState = taskEditorUiState,
             actions = viewModel,
             selectedDate = selectedDate,
-            isNewTask = taskId == null
         )
     }
-
-
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -127,7 +132,6 @@ private fun TaskEditorBottomSheetContent(
     taskEditorState: TaskEditorState,
     selectedDate: LocalDate = Clock.System.now().toLocalDateTime(TimeZone.UTC).date,
     actions: TaskEditorInteractionListener,
-    isNewTask: Boolean
 ) {
 
     val task = taskEditorState.task
@@ -156,158 +160,111 @@ private fun TaskEditorBottomSheetContent(
         }
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Theme.color.surface)
-    ) {
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .verticalScroll(rememberScrollState())
-                .padding(horizontal = 24.dp, vertical = 16.dp)
-        ) {
-            Text(
-                text = if (isNewTask) stringResource(R.string.add_task) else stringResource(R.string.edit_task),
-                color = Theme.color.title,
-                style = Theme.textStyle.title.large,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
-
-            DefaultTextField(
-                textValue = task.title,
-                onValueChange = {
-                    if (it.length <= 30) {
-                        actions.onChangeTaskTitleValue(it)
-                    }
-                },
-                hint = stringResource(R.string.task_title),
-                icon = painterResource(R.drawable.addeditfield)
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            ParagraphTextField(
-                textValue = task.description,
-                onValueChange = {
-                    if (it.length <= 100) {actions.onChangeTaskDescriptionValue(it)}},
-                hint = stringResource(R.string.description)
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            val context = LocalContext.current
-            val formattedDate = task.dueDate.formatAsLocalizedDate(context)
-
-
-            DefaultTextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(16.dp)),
-                isReadOnly = true,
-                onClicked = { showDatePickerDialog = true },
-                textValue = formattedDate,
-                hint = stringResource(R.string.due_date_hint),
-                icon = painterResource(R.drawable.calendar_plus)
-            )
-
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Text(
-                text = stringResource(R.string.priority),
-                color = Theme.color.title,
-                style = Theme.textStyle.title.medium,
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                TaskPriority.entries.reversed().forEach { priority ->
-                    val icon = when (priority) {
-                        TaskPriority.HIGH -> R.drawable.flag
-                        TaskPriority.MEDIUM -> R.drawable.alert
-                        TaskPriority.LOW -> R.drawable.trade_down_icon
-                    }
-                    val selectedBackgroundColor = when (priority) {
-                        TaskPriority.HIGH -> Theme.color.pinkAccent
-                        TaskPriority.MEDIUM -> Theme.color.yellowAccent
-                        TaskPriority.LOW -> Theme.color.greenAccent
-                    }
-                    Priority(
-                        icon = painterResource(icon),
-                        selectedBackgroundColor = selectedBackgroundColor,
-                        label = priority.toStringResource(),
-                        isSelected = task.taskPriority == priority,
-                        modifier = Modifier
-                            .clickable { actions.onChangeTaskPriorityValue(priority) }
-                    )
+    Column {
+        DefaultTextField(
+            textValue = task.title,
+            onValueChange = {
+                if (it.length <= 30) {
+                    actions.onChangeTaskTitleValue(it)
                 }
-            }
+            },
+            hint = stringResource(R.string.task_title),
+            icon = painterResource(R.drawable.addeditfield)
+        )
 
-            Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
-            Text(
-                text = stringResource(R.string.category),
-                color = Theme.color.title,
-                style = Theme.textStyle.title.medium,
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-
-
-            LazyVerticalGrid(
-                columns = GridCells.Adaptive(minSize = 100.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .heightIn(min = 100.dp, max = 400.dp),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                verticalArrangement = Arrangement.spacedBy(24.dp),
-            ) {
-                items(taskEditorState.categories) { category ->
-                    val painter = rememberAsyncImagePainter(model = category.imageUri)
-
-                    CategoryItem(
-                        icon = painter,
-                        categoryName = category.name,
-                        isSelected = task.category.id == category.id,
-                        count = 0,
-                        isShowCount = false,
-                        onClick = { actions.onChangeTaskCategoryValue(category.id) },
-                        modifier = Modifier.fillMaxWidth()
-                    )
+        ParagraphTextField(
+            textValue = task.description,
+            onValueChange = {
+                if (it.length <= 100) {
+                    actions.onChangeTaskDescriptionValue(it)
                 }
-            }
+            },
+            hint = stringResource(R.string.description)
+        )
 
-        }
+        Spacer(modifier = Modifier.height(16.dp))
 
-        Column(
+        val context = LocalContext.current
+        val formattedDate = task.dueDate.formatAsLocalizedDate(context)
+
+        DefaultTextField(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Theme.color.surfaceHigh)
-                .padding(start = 16.dp, end = 16.dp, top = 12.dp, bottom = 12.dp)
+                .clip(RoundedCornerShape(16.dp)),
+            isReadOnly = true,
+            onClicked = { showDatePickerDialog = true },
+            textValue = formattedDate,
+            hint = stringResource(R.string.due_date_hint),
+            icon = painterResource(R.drawable.calendar_plus)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = stringResource(R.string.priority),
+            color = Theme.color.title,
+            style = Theme.textStyle.title.medium,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            PrimaryButton(
-                text = if (isNewTask) stringResource(R.string.add) else stringResource(R.string.save),
-                isLoading = taskEditorState.isLoading,
-                isDisable = !taskEditorState.isValidTask,
-                onClick = { if (isNewTask) actions.addTask(task) else actions.editTask(task) },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp)
-                    .clip(RoundedCornerShape(28.dp))
-            )
+            TaskPriority.entries.reversed().forEach { priority ->
+                val icon = when (priority) {
+                    TaskPriority.HIGH -> R.drawable.flag
+                    TaskPriority.MEDIUM -> R.drawable.alert
+                    TaskPriority.LOW -> R.drawable.trade_down_icon
+                }
+                val selectedBackgroundColor = when (priority) {
+                    TaskPriority.HIGH -> Theme.color.pinkAccent
+                    TaskPriority.MEDIUM -> Theme.color.yellowAccent
+                    TaskPriority.LOW -> Theme.color.greenAccent
+                }
+                Priority(
+                    icon = painterResource(icon),
+                    selectedBackgroundColor = selectedBackgroundColor,
+                    label = priority.toStringResource(),
+                    isSelected = task.taskPriority == priority,
+                    modifier = Modifier
+                        .clickable { actions.onChangeTaskPriorityValue(priority) }
+                )
+            }
+        }
 
-            Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
-            SecondaryButton(
-                text = stringResource(R.string.cancel),
-                isLoading = false,
-                isDisable = false,
-                onClick = actions::cancel,
-                modifier = Modifier.fillMaxWidth()
-            )
+        Text(
+            text = stringResource(R.string.category),
+            color = Theme.color.title,
+            style = Theme.textStyle.title.medium,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 100.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(min = 100.dp, max = 400.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+        ) {
+            items(taskEditorState.categories) { category ->
+                val painter = rememberAsyncImagePainter(model = category.imageUri)
+
+                CategoryItem(
+                    icon = painter,
+                    categoryName = category.name,
+                    isSelected = task.category.id == category.id,
+                    count = 0,
+                    isShowCount = false,
+                    onClick = { actions.onChangeTaskCategoryValue(category.id) },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         }
     }
 }
